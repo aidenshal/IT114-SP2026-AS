75b1237f11bed5f6d5f81040af08d593ae9d622c
aidenshal <128063471+aidenshal@users.noreply.github.com>
2026-02-02 10:23:29 -0500

    fixing lab 1 and adding lab 2 base code

--- Lab01_Setup_AS/Hello.java
+++ Lab01_Setup_AS/Hello.java
@@ -2,7 +2,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        String javaVersion = System.getProperty("java.version");
-        System.out.println("Java Version: " + javaVersion);
     }
 }
